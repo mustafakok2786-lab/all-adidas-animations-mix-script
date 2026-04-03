56930fa4242bcb8c14fb0094cc76aee281a85b4e
mustafakok2786-lab <mustafakok2786@gmail.com>
2026-04-03 20:13:46 +0300

    animation
    Removed duplicate code and added a print statement to confirm script execution.

--- animasyon.lua
+++ animasyon.lua
@@ -1,3 +1,4 @@
+print ("Script is working")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
@@ -8,7 +9,7 @@ local mixedIDs = {
     jump = "rbxassetid://18538153691",       
     fall = "rbxassetid://18538164337",       
     swim = "rbxassetid://106537993816942",   
-    climb = "rbxassetid://93993406355955"    
+    climb = "rbxassetid://93993406355955" ,   
 }
 
 local function setMixed(char)
@@ -30,33 +31,3 @@ if player.Character then
 end
 
 player.CharacterAdded:Connect(setMixed)
-
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local mixedIDs = {
-    idle = "rbxassetid://126354114956642",   
-    walk = "rbxassetid://75183215343859",    
-    run = "rbxassetid://123973978164540",    
-    jump = "rbxassetid://18538153691",       
-    fall = "rbxassetid://18538164337",       
-    swim = "rbxassetid://106537993816942",   
-    climb = "rbxassetid://93993406355955"    
-}
-
-local function setMixed(char)
-    local animate = char:WaitForChild("Animate")
-    
-    animate.idle.Animation1.AnimationId = mixedIDs.idle
-    animate.idle.Animation2.AnimationId = mixedIDs.idle
-    animate.walk.Animation1.AnimationId = mixedIDs.walk
-    animate.run.Animation1.AnimationId = mixedIDs.run
-    animate.jump.Animation1.AnimationId = mixedIDs.jump
-    animate.fall.Animation1.AnimationId = mixedIDs.fall
-    animate.swim.Animation1.AnimationId = mixedIDs.swim
-    animate.swimidle.Animation1.AnimationId = mixedIDs.swim
-    animate.climb.Animation1.AnimationId = mixedIDs.climb
-end
-
-player.CharacterAdded:Connect(setMixed)
-if player.Character then setMixed(player.Character) end
